be1a2d1bab3b99231c129d59adfc0f056893a623
Lívia Santana <livviasantana@gmail.com>
2024-05-25 19:43:55 +0000

    alteração dinâmica da página do quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,12 +1,25 @@
 import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 
-
-
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
-botaoTema.addEventListener("click", () =>{
+botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ícone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
